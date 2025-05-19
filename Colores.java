fca6202df8d6e45c8fbab40170d380e4fdc6cd02
dilan <cervantessalasdilan8@gmail.com>
2025-05-19 08:59:38 -0500

    Cervantes

---
+++ Colores.java
@@ -0,0 +1,9 @@
+public class Colores {
+  public static void main(String[] args) {
+    System.out.println("\033[33m manzana]");
+  }  
+}   System.out.println("\033[32m verde");
+    System.out.println("\033[31mtomate"); 
+    System.out.println("\033[36mcielo");
+    System.out.println("\0[34m33mar");
+    
---
+++ Tabla colores.java
@@ -0,0 +1,24 @@
+ public class TablaColores {
+    public static void main(String[] args) {
+        System.out.println("+--------+---------------+--------+---------------+");
+        System.out.println("| Código | Color         | Código | Color         |");
+        System.out.println("+--------+---------------+--------+---------------+");
+        System.out.println("| \033[30m30m negro     \033[39;49m | \033[90m90m negro claro \033[39;49m |");
+        System.out.println("| \033[31m31m rojo      \033[39;49m | \033[91m91m rojo claro  \033[39;49m |");
+        System.out.println("| \033[32m32m verde     \033[39;49m | \033[92m92m verde claro \033[39;49m |");
+        System.out.println("| \033[33m33m amarillo  \033[39;49m | \033[93m93m amarillo claro \033[39;49m |");
+        System.out.println("| \033[34m34m azul      \033[39;49m | \033[94m94m azul claro  \033[39;49m |");
+        System.out.println("| \033[35m35m morado    \033[39;49m | \033[95m95m morado claro \033[39;49m |");
+        System.out.println("| \033[36m36m cian      \033[39;49m | \033[96m96m cian claro  \033[39;49m |");
+        System.out.println("| \033[37m37m blanco    \033[39;49m | \033[97m97m blanco claro \033[39;49m |");
+        System.out.println("+--------+---------------+--------+---------------+");
+    }   System.out.println("|  31     \033[31m31m rojo \033[39;49m           |");
+        System.out.println("|  91     \033[91m91m rojo claro \033[39;49m     |");
+        System.out.println("|  32     \033[32m32m verde \033[39;49m          |");
+        System.out.println("|  92     \033[92m92m verde claro \033[39;49m    |");
+        System.out.println("|  33     \033[33m33m amarillo \033[39;49m       |");
+        System.out.println("|  93     \033[93m93m amarillo claro \033[39;49m |");
+        System.out.println("|  34     \033[34m34m azul \033[39;49m           |");
+        System.out.println("|  94     \033[94m94m azul claro \033[39;49m     |");
+         
+ 
