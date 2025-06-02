e970c236cdb01f8bb414ac55d926371f9f205eff
dilan <cervantessalasdilan8@gmail.com>
2025-06-02 08:54:44 -0500

    dilan

--- Ejercicios2/Ejercicio2.java
+++
@@ -1,8 +0,0 @@
-package Ejercicios2;
-
-public class Ejercicio2 {
-    public static void main(String[] args) {
-       String nombre= "dilan";
-       System.out.println("Mi nombre es: " +nombre);
-    }
-}
