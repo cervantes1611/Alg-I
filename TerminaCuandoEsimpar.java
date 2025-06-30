41c8a8f998a0c81caeb3e674b42c487c4fadd830
dilan <cervantessalasdilan8@gmail.com>
2025-06-30 09:33:13 -0500

    zzzzz

---
+++ TerminaCuandoEsimpar.java
@@ -0,0 +1,18 @@
+public class TerminaCuandoEsimpar {
+    public static void main(String[] args) {
+        int numero;
+
+        do{
+            System.out.println("Ingresa un numero: ");
+            numero = Integer.parseInt(System.console().readLine());
+
+            if (numero % 2 == 0) {
+         
+        } while (numero % 2 == 0);
+        } else {
+            System.out.println("adsito!!!");
+         } while (numero % 2== 0 ) {
+            
+             
+    }
+}
