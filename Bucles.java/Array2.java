1c25190ddc3533cdec0f7a49cea0be6486f5e06d
dilan <cervantessalasdilan8@gmail.com>
2025-07-17 11:35:14 -0500

    zzzzzz

---
+++ Bucles.java/Array2.java
@@ -0,0 +1,16 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int[] numeros = new int[5]
+
+        System.out.println("Array de Numeros");
+
+        for (int index = 0; index <= 4; i++) {
+            System.out.println("Ingree el numero [" + i + ]");
+            numeros[i] = Integer.parseInt(System.console().readLine( 
+        }
+
+        // que me permita mostrara los datos del array
+        for (int i = 0; i <=4 ; i++) {
+           System.out.println(numeros[i]);
+    }
+}
